8c669567c9eb80cbb89757f00b9b2b3f24f6a6c3
Armando Gil <armando.gil@ntt.eu>
2016-02-29 13:33:05 +0100

    Added new params, virtual alias and rake lint stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,60 +1,58 @@
-# == Class: postfix
-#
-# Full description of class postfix here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'postfix':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
 class postfix (
-    $mynetworks = [ '127.0.0.1' ],
-    $inetinterfaces = '127.0.0.1',
-    $smtpdbanner = '$myhostname ESMTPi $mail_name',
-    $ipv6=false,
-    $relayhost=undef,
-    $opportunistictls=false,
-    $tlscert=undef,
-    $tlspk=undef,
-    $myhostname=undef,
-    $generatecert=false,
-    $subjectselfsigned=undef,
-    )
-    inherits postfix::params {
+    $append_dot_mydomain = $postfix::params::append_dot_mydomain_default,
+    $biff = $postfix::params::biff_default,
+    $generatecert = $postfix::params::generatecert_default,
+    $inetinterfaces = $postfix::params::inetinterfaces_default,
+    $ipv6 = $postfix::params::ipv6_default,
+    $mydestination = $postfix::params::mydestination_default,
+    $mydomain = $postfix::params::mydomain_default,
+    $myhostname = $postfix::params::myhostname_default,
+    $mynetworks = $postfix::params::mynetworks_default,
+    $myorigin = $postfix::params::myorigin_default,
+    $opportunistictls = $postfix::params::opportunistictls_default,
+    $readme_directory = $postfix::params::readme_directory_default,
+    $recipient_delimiter = $postfix::params::recipient_delimiter_default,
+    $relayhost = $postfix::params::relayhost_default,
+    $smtpdbanner = $postfix::params::smtpdbanner_default,
+    $subjectselfsigned = $postfix::params::subjectselfsigned_default,
+    $tlscert = $postfix::params::tlscert_default,
+    $tlspk = $postfix::params::tlspk_default,
+    $virtual_alias = $postfix::params::virtual_alias_default,
+    ) inherits postfix::params {
 
   Exec {
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
   }
 
+  if($virtual_alias)
+  {
+    validate_hash($virtual_alias)
+  }
+
   validate_array($mynetworks)
+
+  if($biff)
+  {
+    validate_string($biff)
+  }
+
+  if($append_dot_mydomain)
+  {
+    validate_string($append_dot_mydomain)
+  }
+
+  validate_string($readme_directory)
+
+  validate_string($myorigin)
+
+  validate_string($mydomain)
+
+  if($recipient_delimiter)
+  {
+    validate_string($recipient_delimiter)
+  }
+
+  validate_array($mydestination)
 
   if($tlscert) or ($tlspk) or ($opportunistictls)
   {
@@ -165,6 +163,26 @@ class postfix (
       unless  => $postfix::params::check_postfix_mta,
       require => [Package[$postfix::params::dependencies], Package['postfix']],
     }
+  }
+
+  if($virtual_alias)
+  {
+
+    exec { 'postmap virtual':
+      command => "postmap ${postfix::params::baseconf}/virtual",
+      creates => "${postfix::params::baseconf}/virtual.db",
+      require => [ File["${postfix::params::baseconf}/virtual"], Package['postfix'] ],
+    }
+
+    file { "${postfix::params::baseconf}/virtual":
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['postfix'],
+      notify  => Service['postfix'],
+      content => template("${module_name}/virtual_alias/virtual_alias.erb"),
+      }
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,26 @@
 class postfix::params {
 
+  $mynetworks_default = [ '127.0.0.1' ]
+  $inetinterfaces_default = '127.0.0.1'
+  $smtpdbanner_default = "${::hostname} ESMTP"
+  $ipv6_default = false
+  $relayhost_default = undef
+  $opportunistictls_default = false
+  $tlscert_default = undef
+  $tlspk_default = undef
+  $myhostname_default = $::hostname
+  $generatecert_default = false
+  $subjectselfsigned_default = undef
+  $biff_default = undef
+  $append_dot_mydomain_default = undef
+  $readme_directory_default = '/usr/share/doc/postfix-2.6.6/README_FILES'
+  $myorigin_default = $::domain
+  $mydomain_default = $::domain
+  $recipient_delimiter_default = undef
+  $mydestination_default = [ $::fqdn, 'localhost' ]
+  $virtual_alias_default = undef
+  $baseconf = '/etc/postfix'
+
   case $::osfamily
   {
     'redhat':
