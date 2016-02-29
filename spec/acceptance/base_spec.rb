e96f93b0f5eda909eedb03994b7d496212331880
Armando Gil <armando.gil@ntt.eu>
2016-02-29 13:33:41 +0100

    Added travis tests

---
+++ spec/acceptance/base_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper_acceptance'
+require_relative './version.rb'
+
+describe 'postfix class' do
+
+  context 'basic setup' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'postfix':
+           mynetworks => [ '127.0.0.0/8', '62.97.117.220/30' ],
+           inetinterfaces => 'all',
+           smtpdbanner => "\$myhostname ESMTP \$mail_name",
+           relayhost => '213.130.46.253',
+           myhostname => $::fqdn,
+           biff => 'no',
+           append_dot_mydomain => 'no',
+           readme_directory => 'no',
+           myorigin => 'test.es',
+           recipient_delimiter => '+',
+           virtual_alias => { '@test.es' => 'root@localhost',}
+        }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it "sleep 10 to make sure postfix is started" do
+      expect(shell("sleep 10").exit_code).to be_zero
+    end
+
+    describe port(25) do
+      it { should be_listening }
+    end
+
+    describe package($packagename) do
+      it { is_expected.to be_installed }
+    end
+
+    describe service($servicename) do
+      it { should be_enabled }
+      it { is_expected.to be_running }
+    end
+
+    it "send test mail" do
+      expect(shell("echo \"Test mail\" | mail test@test.es").exit_code)to be_zero
+    end
+
+    it "check mail reception" do
+      expect(shell("ls /var/spool/mail/root").exit_code)to be_zero
+    end
+
+  end
+
+end
