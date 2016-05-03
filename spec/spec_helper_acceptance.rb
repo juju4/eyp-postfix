3caa028504033436f0e0331fe1ad266a4bf07e26
Jordi Prats <jprats@systemadmin.es>
2016-05-03 11:57:50 +0200

    testing centos 5

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,29 @@
 require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
+require 'beaker_spec_helper'
 
-run_puppet_install_helper
+# require 'beaker/puppet_install_helper'
+# run_puppet_install_helper
+
+hosts.each do |host|
+
+  if host['platform'] =~ /^ubuntu-(15.04|15.10)-/
+    on host, "wget -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb"
+    on host, "dpkg -i --force-all /tmp/puppet.deb"
+    on host, "apt-get update"
+    host.install_package('puppet-agent')
+  else
+    install_puppet_agent_on host, {}
+  end
+
+  # Install git so that we can install modules from github
+  if host['platform'] =~ /^el-5-/
+    # git is only available on EPEL for el-5
+    install_package host, 'epel-release'
+  end
+  install_package host, 'git'
+
+  on host, "puppet cert generate $(facter fqdn)"
+end
 
 RSpec.configure do |c|
   # Project root
