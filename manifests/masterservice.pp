4c0b688a6aaaf7271e3ed87bac3c0475af22f6a3
Jordi Prats <jprats@systemadmin.es>
2016-11-09 17:28:56 +0100

    skel masterservice

---
+++ manifests/masterservice.pp
@@ -0,0 +1,15 @@
+define postfix::masterservice(
+                                $service_type,
+                                $type,
+                                $private,
+                                $unpriv,
+                                $chroot,
+                                $wakeup,
+                                $maxproc,
+                                $command_args,
+                                $comment = undef,
+  ) {
+
+  #service type  private unpriv  chroot  wakeup  maxproc command + args
+
+}
