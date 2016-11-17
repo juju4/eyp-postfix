2a5a5cbfed144c6a4ca076d74c72c9d95a9a3a55
Jordi Prats <jprats@systemadmin.es>
2016-11-17 13:21:00 +0100

    mkdir

--- manifests/vmail/account.pp
+++ manifests/vmail/account.pp
@@ -8,6 +8,22 @@ define postfix::vmail::account(
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
   }
 
+  include ::postfix::vmail
+
+  exec { "eyp-postfix mailbox ${accountname}@${domain}":
+    command => "mkdir -p ${postfix::vmail::mailbox_base}/${domain}/${accountname}",
+    creates => "${postfix::vmail::mailbox_base}/${domain}/${accountname}",
+  }
+
+  file { "${postfix::vmail::mailbox_base}/${domain}/${accountname}":
+    ensure  => 'directory',
+    owner   => $postfix::postfix_username,
+    group   => $postfix::postfix_username,
+    mode    => '0770',
+    require => Exec["eyp-postfix mailbox ${accountname}@${domain}"],
+    before  => Class['postfix::service'],
+  }
+
   concat::fragment{ "/etc/postfix/vmail_mailbox ${account} ${domain}":
     target  => '/etc/postfix/vmail_mailbox',
     order   => $order,
