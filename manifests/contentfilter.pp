bf7b8486d23b3d0e9a1ab14bd9a451d0ee952a0e
Jordi Prats <jprats@systemadmin.es>
2016-11-20 17:28:31 +0100

    lint

--- manifests/contentfilter.pp
+++ manifests/contentfilter.pp
@@ -1,13 +1,12 @@
 class postfix::contentfilter(
-                              $install_amavis=true,
-                              $content_filter='smtp-amavis:[127.0.0.1]:10024'
+                              $install_amavis = true,
+                              $content_filter = 'smtp-amavis:[127.0.0.1]:10024'
                             ) inherits postfix::params {
 
   if($install_amavis)
   {
     class { 'amavis': }
   }
-
 
   concat::fragment{ '/etc/postfix/main.cf content filter':
     target  => '/etc/postfix/main.cf',
