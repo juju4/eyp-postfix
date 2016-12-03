70ff55633fb3c996ea707edaae130dbb08ce9da9
Jordi Prats <jprats@systemadmin.es>
2016-12-03 11:10:05 +0100

    variables per amavis

--- manifests/vmail.pp
+++ manifests/vmail.pp
@@ -14,6 +14,8 @@
 class postfix::vmail(
                       $mailbox_base                 = '/var/vmail',
                       $setup_dovecot                = true,
+                      $setup_amavis                 = false,
+                      $setup_clamav                 = false,
                       #TODO: rewrite
                       $smtpd_recipient_restrictions = [ 'permit_inet_interfaces',
                                                         'permit_mynetworks',
@@ -30,6 +32,13 @@ class postfix::vmail(
                     ) inherits postfix::params {
   Exec {
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
+
+  if($setup_amavis)
+  {
+    class { 'amavis':
+      setup_clamav => $setup_clamav,
+    }
   }
 
   if($setup_dovecot)
