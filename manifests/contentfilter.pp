44e56566388c7f8467732ecbbb468a207fff7222
Jordi Prats <jprats@systemadmin.es>
2016-12-03 11:42:38 +0100

    content filters

--- manifests/contentfilter.pp
+++ manifests/contentfilter.pp
@@ -1,7 +1,8 @@
 class postfix::contentfilter(
                               $install_amavis = true,
                               $install_clamav = true,
-                              $content_filter = 'smtp-amavis:[127.0.0.1]:10024'
+                              $content_filter = 'smtp-amavis:[127.0.0.1]:10024',
+                              $add_instances  = true,
                             ) inherits postfix::params {
 
   if($install_amavis)
@@ -17,18 +18,54 @@ class postfix::contentfilter(
     content => template("${module_name}/contentfilter.erb"),
   }
 
-  # service type  private unpriv  chroot  wakeup  maxproc command + args
-  # smtp      inet  n       -       n       -       -       smtpd
-  #  -o smtpd_sasl_auth_enable=yes
-  #  -o receive_override_options=no_address_mappings
-  #  -o content_filter=smtp-amavis:127.0.0.1:10024
-  #
-  #  smtp-amavis  unix  -    -       y       -       2       smtp
-  #  -o smtp_data_done_timeout=1200
-  #  -o disable_dns_lookups=yes
-  #  -o smtp_send_xforward_command=yes
-  # 127.0.0.1:10025 inet n  -       y       -       -       smtpd
-  #  -o content_filter=
-  #  -o smtpd_helo_restrictions=
-  #  -o smtpd_sender_restrictions=
+  if($add_instances)
+  {
+    # service type  private unpriv  chroot  wakeup  maxproc command + args
+    # smtp      inet  n       -       n       -       -       smtpd
+    #  -o smtpd_sasl_auth_enable=yes
+    #  -o receive_override_options=no_address_mappings
+    #  -o content_filter=smtp-amavis:127.0.0.1:10024
+
+    # no aplico
+
+    #  service      type  private unpriv  chroot  wakeup  maxproc command + args
+    #  smtp-amavis  unix  -          -       y       -       2       smtp
+    #  -o smtp_data_done_timeout=1200
+    #  -o disable_dns_lookups=yes
+    #  -o smtp_send_xforward_command=yes
+
+    postfix::instance { 'smtp-amavis':
+      type => 'unix',
+      chroot => 'y',
+      maxproc => '2',
+      command => 'smtp',
+      opts => {
+        'smtp_data_done_timeout' => '1200',
+        'disable_dns_lookups' => 'yes',
+        'smtp_send_xforward_command' => 'yes',
+      },
+      order => '98',
+    }
+
+    #  service        type  private unpriv  chroot  wakeup  maxproc command + args
+    # 127.0.0.1:10025 inet    n        -       y       -       -       smtpd
+    #  -o content_filter=
+    #  -o smtpd_helo_restrictions=
+    #  -o smtpd_sender_restrictions=
+
+    postfix::instance { '127.0.0.1:10025':
+      type => 'inet',
+      private => 'n',
+      chroot => 'y',
+      command => 'smtpd',
+      opts => {
+        'content_filter' => '',
+        'smtpd_helo_restrictions' => '',
+        'smtpd_sender_restrictions' => '',
+      },
+      order => '99',
+    }
+
+  }
+
 }
