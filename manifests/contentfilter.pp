1e6c4534c1716f71fc475d487628405c97406f92
Jordi Prats <jprats@systemadmin.es>
2016-12-04 20:49:37 +0100

    lint

--- manifests/contentfilter.pp
+++ manifests/contentfilter.pp
@@ -1,5 +1,7 @@
+#
 # referencia amavis: https://www.server-world.info/en/note?os=CentOS_7&p=mail&f=6
 # referencia DSPAM: https://www.kirya.net/articles/setting-up-dspam-as-a-filter-for-postfix-on-debian/
+#
 class postfix::contentfilter(
                               $setup_amavis        = true,
                               $setup_clamav        = true,
