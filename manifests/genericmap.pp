56d9028b3e42362c7d3dff75742a7120827ae377
Jordi Prats <jprats@systemadmin.es>
2017-03-24 18:55:39 +0100

    typo

--- manifests/genericmap.pp
+++ manifests/genericmap.pp
@@ -4,7 +4,7 @@ define postfix::genericmap(
                             $order    = '55',
                           ) {
 
-  concat::fragment{ "${postfix::smtp_generic_maps} ${from} ${to}":
+  concat::fragment{ "${postfix::smtp_generic_maps} ${map_from} ${map_to}":
     target  => $postfix::smtp_generic_maps,
     order   => $order,
     content => template("${module_name}/generic_map.erb"),
