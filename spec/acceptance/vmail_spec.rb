edad38590d48a45d9de621355ea2e43044502582
Jordi Prats <jprats@systemadmin.es>
2017-06-26 14:38:32 +0200

    milora estil testing

--- spec/acceptance/vmail_spec.rb
+++ spec/acceptance/vmail_spec.rb
@@ -12,13 +12,15 @@ describe 'postfix class' do
 
     	class { 'postfix::vmail': }
 
+      postfix::transport { 'systemadmin.es':
+        transport_noop => true,
+        includesubdomains => false,
+        order => '00',
+      }
+
     	postfix::transport { 'example.com':
     		error => 'email to this domain is not allowed',
     	}
-
-      postfix::transport { 'systemadmin.es':
-        transport_noop => true,
-      }
 
     	postfix::vmail::alias { 'd9abea179bca9c44bdafd19c43c8ad55@d9abea179bca9c44bdafd19c43c8ad55.com':
     		aliasto => [ 'd9abea179bca9c44bdafd19c43c8ad55@6cf31108ba1c3fb808e9a276623e87ed.com' ],
