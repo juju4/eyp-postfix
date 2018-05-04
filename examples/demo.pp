361c5c782784c1d6b338f70f2808163587cddb2e
Jordi Prats <jordi.prats@gmail.com>
2018-05-04 11:18:12 +0200

    demo

---
+++ examples/demo.pp
@@ -0,0 +1,13 @@
+class { 'postfix':
+  mynetworks           => [ '127.0.0.0/8' ],
+  ipv6                 => false,
+  inetinterfaces       => 'all',
+  smtpdbanner          => "\\$myhostname ESMTP \\$mail_name",
+  biff                 => false,
+  append_dot_mydomain  => false,
+  readme_directory     => false,
+  myorigin             => 'test.es',
+  recipient_delimiter  => '+',
+  mail_spool_directory => '/tmp',
+  home_mailbox         => '',
+}
