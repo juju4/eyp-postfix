a845ef2f956196e7d3c1933d422ed69f17d024db
Jordi Prats <jordi.prats@gmail.com>
2018-08-08 11:57:50 +0200

    accounts tb per poder comprar

--- examples/multi_instance_vmaildemo.pp
+++ examples/multi_instance_vmaildemo.pp
@@ -10,3 +10,21 @@ postfix::vmail::alias { 'example@saltait.com':
   aliasto       => [ 'exemple@saltait.com' ],
   instance_name => 'instance_2',
 }
+
+postfix::vmail::account { 'example@systemadmin.es':
+  accountname => 'example',
+  domain      => 'systemadmin.es',
+  password    => 'secretpassw0rd',
+}
+
+postfix::vmail::account { 'silvia@systemadmin.es':
+  accountname => 'silvia',
+  domain      => 'systemadmin.es',
+  password    => 'secretpassw0rd2',
+}
+
+postfix::vmail::account { 'marc@systemadmin.es':
+  accountname => 'marc',
+  domain      => 'systemadmin.es',
+  password    => 'secretpassw0rd3',
+}
